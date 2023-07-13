fa9ccdaa38fa6170d4f12b17763c0b04202619c6
theMIslam <valvekakaxa@gmail.com>
2023-07-13 15:03:19 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mywidgets/MyWidget.kt
@@ -0,0 +1,91 @@
+package com.example.mywidgets
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class MyWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        // this is where we receive an intent broadcast
+        val action = intent!!.action ?: ""
+
+        if (context != null && action == "increase") {
+            // update preferences
+            val prefs = context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
+            prefs.edit().putString(
+                "widgetText",
+                ((prefs.getString("widgetText", "0") ?: "0").toInt() + 1).toString()
+            ).apply()
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    // update all widgets
+    private fun updateWidgets(context: Context) {
+        val manager = AppWidgetManager.getInstance(context)
+        val ids = manager.getAppWidgetIds(ComponentName(context, javaClass))
+        // update every widgets
+        ids.forEach { id -> updateAppWidget(context, manager, id) }
+    }
+
+    // this is where we create such an intent
+    private fun pendingIntent(
+        context: Context?,
+        action: String
+    ): PendingIntent? {
+        val intent = Intent(context, javaClass)
+        intent.action = action
+
+
+        // return the pending intent
+        return PendingIntent.getBroadcast(
+            context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+        )
+    }
+
+    internal fun updateAppWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int
+    ) {
+
+        // get the widget text from shared preferences
+        val prefs = context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
+        val widgetText = prefs.getString("widgetText", "0")
+        // Construct the RemoteViews object
+        val views = RemoteViews(context.packageName, R.layout.my_widget)
+        views.setTextViewText(R.id.appwidget_text, widgetText)
+
+        // launch a pending intent to increase the value saved in shared preferences
+        views.setOnClickPendingIntent(R.id.button, pendingIntent(context, "increase"))
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+}
